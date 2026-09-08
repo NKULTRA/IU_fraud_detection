f8f75cde7fa95a9929c8366c114021227cea6d18
Niklas <niklas.gellhorn@googlemail.com>
2026-09-08 23:14:26 +0200

    some initial exploration

---
+++ src/preprocessing.py
@@ -0,0 +1,26 @@
+# columns to drop as these don't suite the use case of the project
+columns_to_drop = [
+    "Make",
+    "AccidentArea",
+    "VehicleCategory",
+    "VehiclePrice",
+    "DriverRating",
+    "AgeOfVehicle",
+    "WitnessPresent",
+    "NumberOfCars",
+    "BasePolicy",
+    "DayOfWeekClaimed",
+    "MonthClaimed",
+    "WeekOfMonthClaimed",
+    "PolicyNumber",
+    "RepNumber",
+    "Fault"
+]
+
+ordinal_columns = [
+
+]
+
+nominal_columns = [
+    
+]
